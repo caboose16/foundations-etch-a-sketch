199e960f27aec611043e3b87bb2adb107790c3c2
Samuel Santana <theSamuelSantana@gmail.com>
2023-09-02 16:54:59 -0700

    Add button to generate board with user input

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ function coverEtchSquare() {
     this.classList.add("etch-square-covered")
 }
 
-var deleteBoard = () => containerDiv.childNodes.forEach( (n) => n.remove() );
+function deleteBoard() {
+    while (containerDiv.firstChild) {
+        containerDiv.removeChild(containerDiv.firstChild);
+    }
+}
 
-function generateCleanBoard(){
-    const rows = 16;
-    const cols = 16;
+function generateCleanBoard(rows, cols){
+    rows ??= 16;
+    cols ??= 16;
 
     deleteBoard();
 
@@ -25,6 +29,22 @@ function generateCleanBoard(){
 
         containerDiv.appendChild(rowDiv)
     }
+}
+
+function newBoard() {
+    let rows = prompt("Please enter the number of rows for the board");
+    if (rows === null) return;
+    if (isNaN(rows) || rows < 1 || rows > 100) {
+        window.alert("Invalid input for rows. Please enter a number from 1-100");
+    }
+
+    let cols = prompt("Please enter the number of columns for the board");
+    if (cols === null) return;
+    if (isNaN(cols) || cols < 1 || cols > 100) {
+        window.alert("Invalid input for cols. Please enter a number from 1-100");
+    }
+
+    generateCleanBoard(rows, cols);
 }
 
 generateCleanBoard();
